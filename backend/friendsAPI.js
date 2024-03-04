00ab7faf95fab395a671ef73fbeb24c28e0cf1fa
ngorli <ngorlip@stanford.edu>
2024-03-03 20:37:46 -0800

    Created the search API

--- backend/friendsAPI.js
+++ backend/friendsAPI.js
@@ -5,28 +5,96 @@ import { FIRESTORE_DB as db } from '../firebaseConfig'
 import { getDatabase, onValue, ref } from "firebase/database";
 
 export const sendFriendRequest = async (requester, requestee) => {
-    // access the friend doc for the requester
-    const q1 = query(collection(db, "users"), where("user_id", "==", requester))
-    const querySnapshot1 = await getDocs(q1);
-    querySnapshot1.forEach((doc) => {
-      requesterDocId = doc.id;
-    });
-    const requesterProfile = doc(db, "users", requesterDocId)
-  
-    // access the friend doc for the requester
-    const q2 = query(collection(db, "users"), where("user_id", "==", requestee))
-    const querySnapshot2 = await getDocs(q2);
-    querySnapshot2.forEach((doc) => {
-      requesteeDocId = doc.id;
-    });
-    const requesteeProfile = doc(db, "users", requesteeDocId)
-  
-    // add the requester to the requestee's firend requests collection
-    requesteeProfile.friend_requests = requesteeProfile.friend_requests.push(requester) 
-  
-    requesterProfile.pending_requests = requesterProfile.pending_requests.push()
-  
-    await addDoc(collection(db, 'recipes'), recipe);
-    console.log('Recipe uploaded successfully');
-  }
-  
+  // access the friend doc for the requester
+  const q1 = query(collection(db, "users"), where("user_id", "==", requester));
+  const querySnapshot1 = await getDocs(q1);
+
+  // get the document and the data
+  const requesterProfileDoc = doc(db, "users", querySnapshot1.docs[0].id);
+  const requesterProfile = querySnapshot1.docs[0].data();
+
+  // access the friend doc for the requester
+  const q2 = query(collection(db, "users"), where("user_id", "==", requestee));
+  const querySnapshot2 = await getDocs(q2);
+
+  // get the document and the data
+  const requesteeProfileDoc = doc(db, "users", querySnapshot2.docs[0].id);
+  const requesteeProfile = querySnapshot2.docs[0].data();
+
+  // add the requester to the requestee's friend requests collection
+  let tempArray = requesteeProfile.friend_requests;
+  tempArray.push(requester)
+  requesteeProfile.friend_requests = tempArray;
+
+
+  // add the requestee to the requester's pending_requests collection
+  tempArray = requesterProfile.pending_requests;
+  tempArray.push(requestee)
+  requesterProfile.pending_requests = tempArray;
+
+  // update the documents
+  await updateDoc(requesteeProfileDoc, requesteeProfile).then(() => {
+    console.log("Document successfully updated");
+  }).catch((error) => {
+    console.error("Error updating document: ", error);
+  });
+
+  await updateDoc(requesterProfileDoc, requesterProfile).then(() => {
+    console.log("Document successfully updated");
+  }).catch((error) => {
+    console.error("Error updating document: ", error);
+  });
+
+  console.log('Friend request sucessfully sent');
+}
+
+export const acceptFriendRequest = async (requester, requestee) => {
+  // access the friend doc for the requester
+  const q1 = query(collection(db, "users"), where("user_id", "==", requester));
+  const querySnapshot1 = await getDocs(q1);
+
+  // get the document and the data
+  const requesterProfileDoc = doc(db, "users", querySnapshot1.docs[0].id);
+  const requesterProfile = querySnapshot1.docs[0].data();
+
+  // access the friend doc for the requester
+  const q2 = query(collection(db, "users"), where("user_id", "==", requestee));
+  const querySnapshot2 = await getDocs(q2);
+
+  // get the document and the data
+  const requesteeProfileDoc = doc(db, "users", querySnapshot2.docs[0].id);
+  const requesteeProfile = querySnapshot2.docs[0].data();
+
+  // remove the requester from friend requests and add to friends
+  let tempArray = requesteeProfile.friends;
+  tempArray.push(requester);
+  requesteeProfile.friends = tempArray;
+  tempArray = requesteeProfile.friend_requests
+  let index = tempArray.indexOf(requester);
+  tempArray.splice(index, 1)
+  requesteeProfile.friend_requests = tempArray
+
+  // remove the requestee from pending and add to friends
+  tempArray = requesterProfile.friends;
+  tempArray.push(requestee);
+  requesterProfile.friends = tempArray;
+  tempArray = requesterProfile.pending_requests
+  index = tempArray.indexOf(requestee);
+  tempArray.splice(index, 1)
+  requesterProfile.pending_requests = tempArray
+
+  // update the documents
+  await updateDoc(requesteeProfileDoc, requesteeProfile).then(() => {
+    console.log("Document successfully updated");
+  }).catch((error) => {
+    console.error("Error updating document: ", error);
+  });
+
+  await updateDoc(requesterProfileDoc, requesterProfile).then(() => {
+    console.log("Document successfully updated");
+  }).catch((error) => {
+    console.error("Error updating document: ", error);
+  })
+
+  console.log("Friend Request Successfully Accepted")
+}
