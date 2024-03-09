2593197009ac4968f63305a7aac61c566987fa36
ngorli <ngorlip@stanford.edu>
2024-03-08 17:47:31 -0800

    created search screen

--- src/screens/SearchScreen/SearchScreen.js
+++ src/screens/SearchScreen/SearchScreen.js
@@ -8,67 +8,94 @@ import {
   SafeAreaView,
   TextInput,
 } from "react-native";
-import { Pressable } from "react-native";
+import { Pressable, Button, FlatList } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { useState, useEffect } from "react";
 import { Themes } from "../../../assets/Themes";
+import { addDoc, collection, query, get, where, getDocs, getFirestore, doc, setDoc, getDoc, updateDoc, FirestoreError } from 'firebase/firestore';
+import { FIRESTORE_DB as db } from '../../../firebaseConfig'
+import { getDatabase, onValue, ref } from "firebase/database";
+import { getPublicRecipes } from "../../../backend/searchAPI";
+import { ScrollView } from "react-native";
+import { Link } from "expo-router";
+
+
+//import { getPublicRecipes } from "../../../backend/searchAPI";
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
 export default function Page() {
-  const [recipeName, setRecipeName] = useState("");
   const [typed, setTyped] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const performSearch = async () => {
+    // Perform search logic here based on the typed query
+    // For demonstration, let's just set some dummy search results
+
+    try {
+      const results = await getPublicRecipes("example user", typed)
+      setSearchResults(results);
+      console.log("You searched for", typed)
+      console.log("Search Results", searchResults)
+    } catch (error) {
+      console.error('Error performing search:', error);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.header}>
-        {
-          //<View style={styles.headerTop}>
-        }
-        <View style={styles.search}>
-          <TextInput
-            style={styles.input}
-            color="black"
-            placeholderTextColor="honeydew"
-            onChangeText={(newText) => setTyped(newText)}
-            placeholder="What are you looking for?"
-          />
-          <Pressable onPress={() => setRecipeName(typed)}>
-            <View>
-              <Ionicons name="search" size={25} color={"black"} />
-            </View>
-          </Pressable>
-        </View>
-      </View>
-      <View style={styles.componentContainer}>
-        <View style={styles.recipeImageContainer}>
-          <Image
-            style={styles.recipeImage}
-            source={require("../../../assets/favicon.png")}
-          />
-        </View>
-        <View style={styles.recipeNameContainer}>
-          <View style={styles.recipeNameHeader}>
-            <Ionicons
-              name="person-circle-outline"
-              size={36}
-              color={Themes.colors.darkShade}
+      <ScrollView>
+        <View style={styles.header}>
+          <View style={styles.search}>
+            <TextInput
+              style={styles.input}
+              placeholderTextColor="honeydew"
+              onChangeText={(newText) => setTyped(newText)}
+              placeholder="What are you looking for?"
+              value={typed}
             />
-            <Text numberOfLines={1} style={styles.recipeName}>
-              User Name
-            </Text>
+            <Pressable onPress={performSearch}>
+              <View>
+                <Ionicons name="search" size={40} color={"black"} />
+              </View>
+            </Pressable>
           </View>
-          <Text numberOfLines={1} style={styles.recipeName}>
-            This is where the recipe name would go
-          </Text>
-          <Text numberOfLines={1} style={styles.recipeName}>
-            This is where the recipe rating would go
-          </Text>
-          <Text numberOfLines={2} style={styles.recipeName}>
-            This is where the recipe description would go
-          </Text>
         </View>
-      </View>
+
+        <View style={styles.searchResultsContainer}>
+          {searchResults.map(result => (
+          <Link
+          href = {{
+            pathname: "/RecipeLanding",
+            params : {
+              recipe: result, // passing the recipe dictionary
+            },
+          }}
+          asChild
+          >
+            <Pressable>
+
+                <View key={result.id} style={styles.searchResult}>
+                  <View style={styles.imageContainer}>
+                    <Image
+                      source={{ uri: result.recipe_picture }} 
+                      style={styles.searchResultImage}
+                    />
+                  </View>
+                  <View style={styles.textContainer}>
+                    <Text style={styles.searchResultName}>{result.recipe_title}</Text>
+                    <Text style={styles.searchResultUser}>{result.user_id}</Text>
+                    <Text style={styles.searchResultDescription}>{result.recipe_description}</Text>
+                  </View>
+                </View>
+              </Pressable>
+            </Link> 
+
+
+          ))}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -77,97 +104,63 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    padding: 24,
     backgroundColor: Themes.colors.background,
   },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  header: {
-    height: "12%",
-    width: "100%",
-    flexDirection: "column",
-    alignContent: "center",
-    borderColor: "red",
-    borderWidth: 10,
-    //justifyContent: "center",
-    paddingHorizontal: 40,
-  },
-  headerTop: {
-    flex: 2,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    borderColor: "yellow",
-    borderWidth: 10,
-  },
   search: {
-    resizeMode: "contain",
-    height: "100%",
     width: "100%",
-    borderRadius: 10,
-    //backgroundColor: "green",
-    borderColor: "green",
-    borderWidth: 10,
+    backgroundColor: Themes.colors.medShade,
+    borderColor: Themes.colors.medShade,
+    borderWidth: 2, // Adjust border width as needed
+    borderRadius: 30, // Adjust border radius as needed
     flexDirection: "row",
     alignItems: "center",
-    //alignContent: "center",
-    justifyContent: "space-around",
+    paddingHorizontal: 10,
   },
-
   input: {
     height: "100%",
-    width: "85%",
-    margin: 7,
-    padding: 15,
-    //color: "black",
+    width: "80%",
+    color: "honeydew",
     fontSize: 20,
   },
-  componentContainer: {
-    // flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    height: windowHeight * 0.15,
+  searchResultsContainer: {
     width: "100%",
-    marginBottom: 15,
-    borderWidth: 5,
-    borderColor: Themes.colors.lightShade,
-    borderRadius: 20,
-    backgroundColor: Themes.colors.boxBackground,
-  },
-  recipeImageContainer: {
-    height: "100%",
-    width: "40%",
-  },
-  recipeImage: {
-    height: "100%",
-    width: "100%",
-    borderTopLeftRadius: 15,
-    borderBottomLeftRadius: 15,
-  },
-  recipeNameContainer: {
-    height: "100%",
-    width: "60%",
-    paddingHorizontal: 5,
-    borderColor: "yellow",
-    borderWidth: 3,
-    //alignItems: "center",
-    justifyContent: "space-around",
-  },
-  recipeNameHeader: {
-    height: "30%",
-    width: "100%",
-    flexDirection: "row",
-    borderColor: "blue",
+    flex: 1,
+    padding: 10,
     borderWidth: 1,
-    alignItems: "center",
+    borderColor: Themes.colors.darkShade,
+    borderRadius: 5,
+    marginTop: 40,
+    marginLeft: 10,
   },
-  recipeName: {
+  searchResult: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20, // Adjust spacing between search results as needed
+  },
+  imageContainer: {
+    marginLeft: 0, // Adjust margin as needed
+    marginRight: 10
+  },
+  searchResultImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+  },
+
+  searchResultName: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 5, // Adjust spacing between title and other text as needed
+    color: "honeydew",
+  },
+  searchResultUser: {
     fontSize: 16,
-    fontWeight: "bold",
-    color: Themes.colors.darkShade,
+    marginBottom: 5, // Adjust spacing between user and description as needed
+    color: "honeydew",
+  },
+  searchResultDescription: {
+    fontSize: 14,
+    width: "60%",
+    color: "honeydew",
   },
 });
